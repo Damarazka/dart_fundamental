e0f56a54aae3590b17aaa5c9471381df10337d23
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-01-25 17:49:02 +0700

    loop

---
+++ test/loop.dart
@@ -0,0 +1,18 @@
+void main() {
+/* for (var i = 0; i <= 10 ; i+=10) {
+print(i);
+} */
+
+  int awal = 0;
+  int akhir = 5;
+
+  /* while (awal < akhir) {
+    awal++;
+    print("aah $awal");
+  } */
+
+  do {
+    awal++;
+    print("awal $awal");
+  } while (awal < akhir);
+}
